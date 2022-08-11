ea3501fb1b71421efa421a912e73510e202e79f3
Sai Meher Borra <sai.meher@kanerika.com>
2022-08-11 15:50:36 +0530

    made final changes

--- src/main/java/com/geeson/annotations/AnnotationsApplication.java
+++ src/main/java/com/geeson/annotations/AnnotationsApplication.java
@@ -1,7 +1,6 @@
 package com.geeson.annotations;
 
 import com.google.gson.Gson;
-import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -20,37 +19,12 @@ public class AnnotationsApplication {
         //SpringApplication.run(AnnotationsApplication.class, args);
         Employee employee = new Employee("Meher", "SDE", 123, 20, 100000);
         SameThing sameThing = new SameThing();
-        //String jsonString = sameThing.convertToJson(employee);
-        //System.out.println(jsonString);
-        //Employee employee1 = (Employee) sameThing.jsonToObject(new Employee());
-        //System.out.println(employee1.toString());
-
-
-        String query = "insert into STUDENT_DATA (first_name, last_name, gender, country_name, email, password) values (?,?,?,?,?,?)";
-        insertElement(query, employee);
-
+        String jsonString = sameThing.convertToJson(employee);
+        System.out.println(jsonString);
+        Employee employee1 = (Employee) sameThing.jsonToObject(new Employee());
+        System.out.println(employee1.toString());
     }
-
-    public static void insertElement(String query, Employee emp) throws ClassNotFoundException, SQLException {
-        String url = "jdbc:postgresql://localhost:5432/test";
-        String userName = "postgres";
-        String password = "Kyouma#001";
-
-        Class.forName("org.postgresql.Driver");
-        Connection con = DriverManager.getConnection(url, userName, password);
-        PreparedStatement preparedStatement = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1,emp.getName());
-        preparedStatement.setString(2,emp.getName());
-        preparedStatement.setString(3,emp.getJobTitle());
-        preparedStatement.setString(4,"India");
-        preparedStatement.setString(5,"email@email.com");
-        preparedStatement.setString(6,"HyvdcUbc");
-
-        int affectedRows = preparedStatement.executeUpdate();
-        System.out.println(affectedRows);
-        con.close();
-    }
-    /*public static void serializedEmployee(){
+    public static void serializedEmployee(){
         Employee employee = new Employee("Meher", "SDE", 123, 20, 100000);
         Gson gson = new Gson();
         String json = gson.toJson(employee);
@@ -63,6 +37,6 @@ public class AnnotationsApplication {
         Gson gson = new Gson();
         Employee employee = gson.fromJson(content, Employee.class);
         System.out.println(employee.toString());
-    }*/
+    }
 
     }
